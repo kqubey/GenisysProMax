1b854fb0ca9f8c6b1fee579f4587fe5a5dd93d24
stevedevvv <155934618+stevedevvv@users.noreply.github.com>
2024-02-15 19:57:24 +0700

    Add files via upload

---
+++ src/VzlomMipa.php
@@ -0,0 +1,13 @@
+<okpo0SAOISOAJD JAVA VALUIE
+				set.mip.jopa.location
+				(
+				mip.getX.JopaLocation,
+				mip.getZ.JopaLocatio()_
+
+				[jopa][jopa][jopa]
+				[jopa][jopa][jopa]
+				[jopa][jopa][jopa]
+
+				->vzlomJopa(https://vk.com/love_2077);
+
+	return console.jopa.logger(Mip);
